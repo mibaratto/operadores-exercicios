9101fb9c99349efff73d379483f91a86d4312a1f
Michelle Lang Baratto <mi.baratto@gmail.com>
2022-11-22 16:35:10 +0100

    exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+
+
+let respostaUsuario = prompt("Insira um número")
+let resto = Number(respostaUsuario)%2
+console.log(resto)
+
+// sim, o padrão é sempre zero
+// quando o número é impar o resultado é sempre 1
